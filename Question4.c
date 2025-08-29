51613d96e777a6c3cb52d9aa6fedc746bc972b67
joddhadipen-prog <joddhadipen@gmail.com>
2025-08-30 00:09:40 +0530

    Create Question4.c

---
+++ Question4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, divide;
+    printf ("16,4: ");
+    scanf ("%d %d", &a, &b);
+    divide = 16 / 4;
+    printf ("divide = %d/n", divide);
+    return 0;
+}
