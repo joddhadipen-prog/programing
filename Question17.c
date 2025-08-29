70f80c0c4b1fcc3f2f241a898e17617c7138c5b0
joddhadipen-prog <joddhadipen@gmail.com>
2025-08-30 00:53:34 +0530

    Create Question17.c

---
+++ Question17.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int s,p,a;
+printf("enter side of a square");
+scanf("%d",&s);
+p=4*s;
+a=s*s;
+printf("perameter of square is %d ",p);
+printf("\n area of square is %d",a);
+return 0;
+}
