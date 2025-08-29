41838c02546b854713e8511abf17f2936de15ec4
joddhadipen-prog <joddhadipen@gmail.com>
2025-08-30 01:02:02 +0530

    Create Question20.c

---
+++ Question20.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ int main()
+{
+float area,h,l;
+printf("enter height and length of triangle\n");
+scanf("%f %f", &h, &l);
+area=(h*l)/2;
+printf("area of triangle=%f", area );
+return 0;
+}
