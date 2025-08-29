0180d9b1ff6b7d7887246835ff579b4685679d62
joddhadipen-prog <joddhadipen@gmail.com>
2025-08-29 23:50:10 +0530

    Question2.c

---
+++ Question2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, subtraction;
+    printf ("16 ,12: ");
+    scanf ("%d %d", &a, &b);
+    subtraction = 16 - 12;
+    printf ("subtraction = %d/n", subtraction );
+    return 0;
+}
