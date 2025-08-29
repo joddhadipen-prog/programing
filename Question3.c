56602a064b583c7ba27d084c77a165b0c0a95f79
joddhadipen-prog <joddhadipen@gmail.com>
2025-08-29 23:52:12 +0530

    Question3.c

---
+++ Question3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, multiply;
+    printf ("16,10: ");
+    scanf ("%d %d", &a, &b);
+    multiply = 16 * 10;
+    printf ("mmultiplymultiply = %d/n", multiply );
+    return 0;
+}
