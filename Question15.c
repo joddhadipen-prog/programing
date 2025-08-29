99af85fb5f9b8b8af6ede969c99b329c11c7c992
joddhadipen-prog <joddhadipen@gmail.com>
2025-08-30 00:48:30 +0530

    Create Question15.c

---
+++ Question15.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+float celsius,fahrenhite;
+printf("enter fahrenhite\n");
+scanf("%f",&fahrenhite );
+celsius=(5.0/9.0)*(fahrenhite-32);
+printf(" in celsius is %f\n",celsius);
+return 0;
+}
